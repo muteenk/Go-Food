b7ab629a29d9d8e530f68e7526df0a1e31a29879
muteenk <kundangarmuteen@gmail.com>
2024-06-22 23:21:38 +0530

    Updated nav responsiveness

--- app/components/Home/Hero.tsx
+++ app/components/Home/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
                 <h1 className="font-sec text-[3.5em] font-bold text-darkText leading-tight">Buy Fresh And Organic Grocery Food</h1>
                 <p className="text-[18px] font-semibold text-[#d8d8d8] my-11"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque minus animi veritatis, molestias, id repellendus cumque sed quidem consequuntur!</p>
                 <div className="flex justify-between items-center w-full">
-                    <button className="group bg-priColor text-[white] font-medium px-[20px] py-[10px] rounded-full mx-3 mr-14">SHOP NOW <i className="group-hover:translate-x-2 translate-all duration-500 fa-solid fa-arrow-right text-sm mx-3"></i></button>
+                    <button className="group bg-priColor text-[white] font-medium px-[25px] py-[13px] rounded-full mx-3 mr-14">SHOP NOW <i className="group-hover:translate-x-2 translate-all duration-500 fa-solid fa-arrow-right text-sm mx-3"></i></button>
                     <div className="flex flex-col justify-center items-center mx-11">
                         <h1 className="font-sec text-darkText text-[2.5em] font-bold">35k +</h1>
                         <p className="text-grayText font-sec font-bold text-[1.3em]">Users</p>
@@ -27,10 +27,10 @@ const Hero = () => {
         </div>
 
         <div className="relative flex justify-center items-center bg-priColor w-[45%]  h-full">
-            <Image src={FoodPlate} alt="grocery" width={1920} height={1080} className="absolute top-[5em] left-[-17em] z-20 w-[140em] drop-shadow-[-3px_2px_1px_rgba(0,0,0,0.7)]"/>
-            <div className="absolute top-[10em] left-[-12em] z-10 h-[44.5em] w-[44.5em] bg-priColor border-[7.6px] border-transparentWhite rounded-full"></div>
-            <Image src={Leaf} alt="Leaf" height={300} width={100} className="absolute bottom-[10em] left-[32em] z-20 w-[3.4em] rotate-[-65deg]" />
-            <Image src={Leaf} alt="Leaf" height={300} width={100} className="absolute bottom-[8em] left-[30em] z-20 w-[2.4em] rotate-[-28deg]" />
+            <Image src={FoodPlate} alt="grocery" width={1920} height={1080} className="absolute top-[55%] translate-y-[-50%] left-[-13em] z-20 w-[48em] drop-shadow-[-3px_2px_1px_rgba(0,0,0,0.7)]"/>
+            <div className="absolute top-[58%] left-[-7em] translate-y-[-50%] z-10 h-[70%] w-[80%] bg-priColor border-[7.6px] border-transparentWhite rounded-full"></div>
+            <Image src={Leaf} alt="Leaf" height={300} width={100} className="absolute bottom-[10em] left-[65%] z-20 w-[3.4em] rotate-[-65deg]" />
+            <Image src={Leaf} alt="Leaf" height={300} width={100} className="absolute bottom-[8em] left-[61%] z-20 w-[2.4em] rotate-[-28deg]" />
         </div>
     </section>
   )
