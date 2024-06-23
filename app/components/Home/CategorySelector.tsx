01501582fe5aa4c040e84b7176034cdc2942f9e9
muteenk <kundangarmuteen@gmail.com>
2024-06-23 20:18:06 +0530

    Added a select option

--- app/components/Home/CategorySelector.tsx
+++ app/components/Home/CategorySelector.tsx
@@ -7,13 +7,27 @@ type CategorySelectorProps = {
 
 const CategorySelector = ({category, setCategory}: CategorySelectorProps) => {
   return (
-    <div className="flex flex-wrap justify-center items-center mt-1 mb-12">
+    <>
+      <div className="hidden sm:flex flex-wrap justify-center items-center mt-1 mb-12">
         <button onClick={() => {setCategory("fruits")}} className={((category === "fruits") ? "bg-priColor text-[white]" : "bg-softGray text-darkText") + " hover:bg-priColor hover:text-[white] font-medium px-[36px] py-[15px] rounded-full mx-3 my-3 transition-all duration-300"}>FRUITS AND VEGETABLES</button>
         <button onClick={() => {setCategory("bakery")}} className={((category === "bakery") ? "bg-priColor text-[white]" : "bg-softGray text-darkText") + " hover:bg-priColor hover:text-[white] font-medium px-[36px] py-[15px] rounded-full mx-3 my-3 transition-all duration-300"}>BAKERY</button>
         <button onClick={() => {setCategory("meat")}} className={((category === "meat") ? "bg-priColor text-[white]" : "bg-softGray text-darkText") + " hover:bg-priColor hover:text-[white] font-medium px-[36px] py-[15px] rounded-full mx-3 my-3 transition-all duration-300"}>MEAT AND SEAFOOD</button>
         <button onClick={() => {setCategory("beverage")}} className={((category === "beverage") ? "bg-priColor text-[white]" : "bg-softGray text-darkText") + " hover:bg-priColor hover:text-[white] font-medium px-[36px] py-[15px] rounded-full mx-3 my-3 transition-all duration-300"}>BEVERAGE</button>
         <button onClick={() => {setCategory("snacks")}} className={((category === "snacks") ? "bg-priColor text-[white]" : "bg-softGray text-darkText") + " hover:bg-priColor hover:text-[white] font-medium px-[36px] py-[15px] rounded-full mx-3 my-3 transition-all duration-300"}>BISCUIT AND SNACKS</button>
-    </div>
+      </div>
+      <div className='flex sm:hidden justify-center items-center'>
+        <select onChange={(e) => {
+          setCategory(e.target.value);
+        }}
+        className='w-[80%] border-[1px] border-priColor rounded-md px-4 py-3 text-priColor'>
+          <option value="fruits">FRUITS AND VEGETABLES</option>
+          <option value="bakery">BAKERY</option>
+          <option value="meat">MEAT AND SEAFOOD</option>
+          <option value="beverage">BEVERAGE</option>
+          <option value="snacks">BISCUIT AND SNACKS</option>
+        </select>
+      </div>
+    </>
   )
 }
 
